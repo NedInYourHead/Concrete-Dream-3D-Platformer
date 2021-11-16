ac29bfa77302ea5cffcbe3ae11b78f165fe49fa1
NedInYourHead <nedcorney@gmail.com>
2021-11-16 04:45:39 -0800

    -Added Strafing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float maxSpeed = 0.05f;
+    public float maxSpeed = 0.05f;
+    public float strafeSpeed = 1.0f;
     float rotation = 0.0f;
     float camRotation = 0.0f;
-    float rotationSpeed = 2.0f;
+    public float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     GameObject cam;
 
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+        transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal") * strafeSpeed);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, -rotation, 0.0f));
