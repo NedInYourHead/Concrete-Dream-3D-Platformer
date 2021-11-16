c421b6b272d63e5a28df6978e696b1bfb070ad69
NedInYourHead <nedcorney@gmail.com>
2021-11-16 06:54:05 -0800

    -Added Jumping (not tweaked)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,19 +11,36 @@ public class PlayerController : MonoBehaviour
     public float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
         transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
         transform.position = transform.position + (transform.right * Input.GetAxis("Horizontal") * strafeSpeed);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, -rotation, 0.0f));
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
